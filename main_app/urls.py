1dc64d4341c9086e0e29927fdab85cc8d5bdd4ad
andywtse <atse4995@email.com>
2022-08-07 11:25:25 -0400

    Adds part 1

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
+  path('swallows/', views.swallows_index, name='swallows_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -8,4 +8,22 @@ def home(request):
   return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
 
 def about(request):
-  return HttpResponse('<h1>Swallows</h1>')
+  return render(request, 'about.html')
+
+def swallows_index(request):
+  return render(request, 'swallows/index.html', {'swallows':swallows})
+
+class Swallow:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, type, description, age, speed):
+    self.name = name
+    self.breed = type
+    self.description = description
+    self.age = age
+    self.speed = speed
+
+swallows = [
+  Swallow('Lolo', 'tabby', 'Kinda rude.', 3, 5),
+  Swallow('Sachi', 'tortoiseshell', 'Looks like a turtle.', 5,5),
+  Swallow('Fancy', 'bombay', 'Happy fluff ball.', 4,5),
+  Swallow('Bonk', 'selkirk rex', 'Meows loudly.', 6,5)
+]
