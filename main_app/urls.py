36c9da1da445e7ededf036a7ba81c35e8b9fb478
andywtse <atse4995@email.com>
2022-08-07 14:51:10 -0400

    Finishes part 2

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('swallows/', views.swallows_index, name='swallows_index'),
+  path('swallows/<int:swallow_id>/', views.swallows_detail, name='swallows_detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,29 +1,17 @@
 from django.shortcuts import render
-
-# Add the following import
-from django.http import HttpResponse
+from .models import Swallow
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def swallows_index(request):
+  swallows = Swallow.objects.all()
   return render(request, 'swallows/index.html', {'swallows':swallows})
 
-class Swallow:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, type, description, age, speed):
-    self.name = name
-    self.breed = type
-    self.description = description
-    self.age = age
-    self.speed = speed
-
-swallows = [
-  Swallow('Lolo', 'tabby', 'Kinda rude.', 3, 5),
-  Swallow('Sachi', 'tortoiseshell', 'Looks like a turtle.', 5,5),
-  Swallow('Fancy', 'bombay', 'Happy fluff ball.', 4,5),
-  Swallow('Bonk', 'selkirk rex', 'Meows loudly.', 6,5)
-]
+def swallows_detail(request, swallow_id):
+  swallow = Swallow.objects.get(id=swallow_id)
+  return render(request, 'swallows/detail.html', { 'swallow': swallow })
